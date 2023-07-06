066329b9c74e1b7a13056ad636254accc546f6a3
Dhamdeva <dhamdeva123@gmail.com>
2023-07-06 12:04:19 +0530

    first commit

--- src/dashbord/homepage/Homepage.tsx
+++ src/dashbord/homepage/Homepage.tsx
@@ -1,20 +1,22 @@
-import { Navbar } from "./Navbar";
-import { DrawerComponent } from "./DrawerComponent";
-import MainNavigator from "../../controller/MainNavigator";
+import { Navbar } from './Navbar'
+import { DrawerComponent } from './DrawerComponent'
+import MainNavigator from '../../controller/MainNavigator'
 
-
-export function Homepage(){
-  console.log(" This is Home");
-  return(
+export function Homepage() {
+  console.log(' This is Home')
+  return (
     <div className="container-fluid p-0">
       <nav>
         <Navbar />
       </nav>
       <div className="d-flex">
-        <div className="sideNav" style={{width:'20%'}}>
+        <div className="sideNav" style={{ width: '20%' }}>
           <DrawerComponent />
         </div>
-        <div className="main" style={{width:'80%',background:'#77b5d9'}}>
+        <div
+          className="main"
+          style={{ width: '80%', background: '#77b5d9' }}
+        >
           <MainNavigator />
         </div>
       </div>
